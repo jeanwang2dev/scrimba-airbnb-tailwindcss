65a8f59f398997da39769d411c04c07094365ff4
jeanwang2dev <jeanharmsen@yahoo.com>
2023-05-03 17:04:11 -0400

    add badge on card

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ const App = () => {
     const cards = data.map( item => {
         return (
           <Card 
-              key={item.id}
-              img={item.coverImg}
-              rating={item.stats.rating}
-              reviewCount={item.stats.reviewCount}
-              location={item.location}
-              title={item.title}
-              price={item.price}
+            //   key={item.id}
+            //   img={item.coverImg}
+            //   rating={item.stats.rating}
+            //   reviewCount={item.stats.reviewCount}
+            //   location={item.location}
+            //   title={item.title}
+            //   price={item.price}
+            key={item.id}
+            {...item}
           />
         )
       });
     return (
-        <div className="max-w-xl mx-auto pb-8 bg-yellow-300">
+        <div className="max-w-xl mx-auto pb-2">
             <Nav />
             <Hero />
             <section className="px-9 flex flex-nowrap overflow-x-scroll gap-x-8">
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,22 @@
 import starimg from '../img/star.png';
 
 export default function Card(props) {
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     //console.log(props);
     return (
         <>
-            <div className="card w-fit text-xs shrink-0 pb-5">
-                <img className="mb-2" src={require(`../img/${props.img}`)} alt="card feature" />
+            <div className="card relative w-fit text-xs shrink-0 pb-5">
+                { badgeText && <div className="absolute top-1.5 left-1.5 rounded-sm bg-white text-black p-1.5 font-semibold">{badgeText}</div>}
+                <img className="mb-2" src={require(`../img/${props.coverImg}`)} alt="card feature" />
                 <div className="flex justify-items-start items-baseline space-x-1 mb-2">
                     <img className="w-3.5 h-3.5" src={starimg} alt="" />
-                    <span>{props.rating}</span>
-                    <span className="text-slate-500">({props.reviewCount}) • </span>
+                    <span>{props.stats.rating}</span>
+                    <span className="text-slate-500">({props.stats.reviewCount}) • </span>
                     <span className="text-slate-500">{props.country}</span>
                 </div>
                 <p className="overflow-hidden leading-3 mb-2">{props.title}</p>
