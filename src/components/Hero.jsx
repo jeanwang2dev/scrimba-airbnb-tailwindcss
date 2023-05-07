d4a4502e098ca231e281eccc0821c76488698502
jeanwang2dev <jeanharmsen@yahoo.com>
2023-05-07 19:50:24 -0400

    adjust hero pic size on small screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ import pics from '../img/masonry.png';
 function Hero() {
     return (      
         <section className="container flex flex-col pt-7 pl-9 items-start mb-5">  
-            <div className="w-full mr-9">
-                <img src={pics} className="w-96 mb-8 mx-auto" alt="airbnb views" />
+            <div className="w-full">
+                <img src={pics} className="w-72 mb-8 mx-auto md:w-96" alt="airbnb views" />
             </div>       
             <h1 className="font-semibold text-4xl mb-4">Online Experiences</h1>
             <p className="text-base font-light leading-tight max-w-xs">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
